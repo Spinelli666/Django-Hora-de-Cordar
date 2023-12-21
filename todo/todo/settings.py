7aa2e4dd7a30ff00bbde534d7a8a721b817f5d2a
Bernardo Gonçalves Spinelli Martins <107887848+Spinelli666@users.noreply.github.com>
2023-12-21 20:03:25 -0300

    Busca

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'tasks',
     'about',
     'users',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
